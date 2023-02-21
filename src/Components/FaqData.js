cc6848d5725a3f87f738c7224ece8ea5d7417f6f
joshjay05 <adeoluwa295@gmail.com>
2023-02-21 14:06:16 +0100

    first commit

---
+++ src/Components/FaqData.js
@@ -0,0 +1,33 @@
+const FAQS = [
+  {
+    id: "1",
+    title:
+      "I do not have any technical background, can I join the course?",
+    body: "You do not need any technical background or previous experience to join our programs. All we require is your willingness to learn and availability for the classes. Our facilitators will be sure to guide you through.",
+  },
+  {
+    id: "2",
+
+    title: "Do you offer any payment plan?",  //payments options
+    body: "We have 3 different payment plans available to intending students. Kindly check the Tuition and payment options available on the homepage.",
+  },
+  {
+    id: "3",
+
+    title: "What is the class schedule?",
+    body: "Weekday classes are held twice a week from Tuesday virtual and Wednesday physical between 10AM and 4PM while the weekend classes are held on Fridays and Saturdays 10AM – 4PM.",
+  },
+  {
+    id: "4",
+
+    title: "Do you offer career support?",
+    body: "Yes, we are happy to offer support to outstanding students in their career journey.",
+  },
+  {
+    id: "5",
+    title: "Do I get a certificate?",
+    body: "Yes you get a certificate of completion after training.",
+  },
+];
+
+export default FAQS;
